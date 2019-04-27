069bcf82fe2c64e6f8049000c7c133aac8f6313b
DLo930 <dlo2@andrew.cmu.edu>
2019-04-27 09:10:43 -0400

    Add hero delete functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -49,6 +49,19 @@ export class HeroService {
     );
   }
 
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const httpOptions = {
+      headers : new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+    const id = (typeof hero === 'number') ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
